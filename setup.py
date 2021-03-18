1ad94bddb00201e81777fa83ec88c580735f9aee
pony ma <pony.ma@student.com>
2021-03-18 15:18:37 +0800

    build(schedulers): Update dir structure

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 
 setup(
     name="nameko-apscheduler",
-    version="1.0.0",
-    description="",
+    version="0.0.1",
+    author="Pony Ma",
+    author_email="mtf201013@gmail.com",
+    description="nameko apscheduler dependency.",
     url="https://github.com/ma-pony/nameko-apscheduler",
     install_requires=[
-        "marshmallow==3.6.0",
-        "nameko==3.0.0-rc8",
-        "kombu==4.6.8",
-        "APScheduler==3.6.3",
+        "marshmallow>=3.6.0",
+        "nameko>=3.0.0-rc8",
+        "kombu>=4.6.8",
+        "APScheduler>=3.6.3",
     ],
     zip_safe=True,
 )
