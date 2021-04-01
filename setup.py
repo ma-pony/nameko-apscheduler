822947b247e81bdf835df2472cd55720529cd876
pony ma <pony.ma@student.com>
2021-04-01 11:23:54 +0800

    build(setup): upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nameko-apscheduler",
-    version="0.0.7",
+    version="0.0.8",
     author="Pony Ma",
     author_email="mtf201013@gmail.com",
     description="nameko apscheduler dependency.",
